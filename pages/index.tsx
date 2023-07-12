5d2089f0b34ed99db2cebb46ce968afd7fe1ca70
Justin Lee <justinleeyile@gmail.com>
2023-07-12 10:36:21 +0800

    added comments

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,36 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import styles from '../styles/Home.module.css'
-import AddressForm from '../components/AddressForm'
-import * as Web3 from '@solana/web3.js'
+import type { NextPage } from "next";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import AddressForm from "../components/AddressForm";
+import * as Web3 from "@solana/web3.js";
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState("");
 
-  const addressSubmittedHandler = (address: string) => {
-    try {
-      setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
-      connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
-      })
-    } catch (error) {
-      setAddress('')
-      setBalance(0)
-      alert(error)
-    }
-  }
+  // There's something wrong with this code, fix it
+  const addressSubmittedHandler = async (address: string) => {
+    // The variable address contain the pubkey of the user in type "String"
+    setAddress(address);
+
+    // Currently the balance state is hardcoded to the value 1000
+    // Your task is to fetch the user current balance and update the state with the correct value
+    setBalance(1000);
+
+    // Challenge 1: Use the try-catch block to handle error and display the error using alert(error)
+    // Challenge 2: Add wallet adapter. Hint => Google "Solana wallet adapter"
+  };
 
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
-        <p>
-          Start Your Solana Journey
-        </p>
+        <p>Start Your Solana Journey</p>
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
